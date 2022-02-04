d1fb138a8fb658e4efa867d3342d8e911b2cb60a
Mridula Zaman <mridula.zaman95@gmail.com>
2022-02-04 13:05:10 -0800

    completed the basic functionalities and requirements

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
+const X = "X";
+const O = "O";
 const PLAYER_X = "Player X";
 const PLAYER_O = "Player O";
+const PLAYER_X_WIN_MSG = "Player X wins!";
+const PLAYER_O_WIN_MSG = "Player O wins!";
+const TIE_MSG = "It's a tie!";
 
 window.onload = function () {
   let count = 0;
-  let changeTurnsButton = document.getElementById("changeTurnsButton");
-  let currentPlayerName = document.getElementById("currentPlayerName");
-
-  changeTurnsButton.onclick = function () {
-    if (count % 2 === 0) {
-      currentPlayerName.innerHTML = PLAYER_O;
-    } else {
-      currentPlayerName.innerHTML = PLAYER_X;
-    }
-    count++;
-  };
+  let currentPlayer = document.getElementById("currentPlayer");
+  let winner = document.getElementById("winner");
 
   let gameBoard = document.getElementById("gameBoard");
   let table = document.createElement("table");
@@ -25,14 +21,85 @@ window.onload = function () {
       var col = row.insertCell(j);
       var button = document.createElement("button");
       button.innerHTML = "-";
-      button.setAttribute("id", idCount);
+      button.setAttribute("class", "row-" + (i + 1) + " column-" + (j + 1));
+      button.setAttribute("id", "btn-" + idCount);
+      button.setAttribute("value", 0);
       col.appendChild(button);
       idCount++;
     }
   }
   gameBoard.appendChild(table);
-};
 
-// Next steps: on click of button show x or o depending on turn and disable the button.
-// Then, code for determining if there is a winner. Next, display winner or tie.
-// Finally add console statements, comments, and styling. Update README.md pagee.
+  const buttonArray = document.querySelectorAll(`[id^="btn-"]`);
+  for (let i = 0; i < buttonArray.length; i++) {
+    buttonArray[i].addEventListener("click", function () {
+      if (count % 2 === 0) {
+        buttonArray[i].innerHTML = "X";
+        buttonArray[i].setAttribute("value", 1);
+        currentPlayer.innerHTML = PLAYER_O;
+      } else {
+        buttonArray[i].innerHTML = "O";
+        buttonArray[i].setAttribute("value", -1);
+        currentPlayer.innerHTML = PLAYER_X;
+      }
+      buttonArray[i].disabled = true;
+      checkForWinner();
+      count++;
+    });
+  }
+
+  function checkForWinner() {
+    if (count === 8) {
+      winner.innerHTML = TIE_MSG;
+    }
+
+    // Check for rows:
+    for (let i = 1; i <= 3; i++) {
+      let row = document.querySelectorAll(`[class^="row-${i}"]`);
+      // Converts nodelist to array:
+      let rowArray = Array.from(row);
+      let rowCount = 0;
+      for (let i = 0; i < 3; i++) {
+        rowCount += parseInt(rowArray[i].value);
+        if (rowCount == 3) {
+          winner.innerHTML = PLAYER_X_WIN_MSG;
+        } else if (rowCount == -3) {
+          winner.innerHTML = PLAYER_O_WIN_MSG;
+        }
+      }
+    }
+
+    // Check for columns:
+    for (let i = 1; i <= 3; i++) {
+      let column = document.querySelectorAll(`[class*="column-${i}"]`);
+      // Converts nodelist to array:
+      let columnArray = Array.from(column);
+      let columnCount = 0;
+      for (let i = 0; i < 3; i++) {
+        columnCount += parseInt(columnArray[i].value);
+        if (columnCount == 3) {
+          winner.innerHTML = PLAYER_X_WIN_MSG;
+        } else if (columnCount == -3) {
+          winner.innerHTML = PLAYER_O_WIN_MSG;
+        }
+      }
+    }
+
+    // Check for diagonals:
+    let criss =
+      parseInt(buttonArray[0].value) +
+      parseInt(buttonArray[4].value) +
+      parseInt(buttonArray[8].value);
+
+    let cross =
+      parseInt(buttonArray[2].value) +
+      parseInt(buttonArray[4].value) +
+      parseInt(buttonArray[6].value);
+
+    if (criss == 3 || cross == 3) {
+      winner.innerHTML = PLAYER_X_WIN_MSG;
+    } else if (criss == -3 || cross == -3) {
+      winner.innerHTML = PLAYER_O_WIN_MSG;
+    }
+  }
+};
