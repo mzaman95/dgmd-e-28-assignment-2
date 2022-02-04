fcb934746215c099cc09506266d1e17da7f03ada
Mridula Zaman <mridula.zaman95@gmail.com>
2022-02-03 21:36:39 -0800

    basics of board

--- script.js
+++ script.js
@@ -14,4 +14,25 @@ window.onload = function () {
     }
     count++;
   };
+
+  let gameBoard = document.getElementById("gameBoard");
+  let table = document.createElement("table");
+
+  let idCount = 0;
+  for (let i = 0; i < 3; i++) {
+    var row = table.insertRow(i);
+    for (let j = 0; j < 3; j++) {
+      var col = row.insertCell(j);
+      var button = document.createElement("button");
+      button.innerHTML = "-";
+      button.setAttribute("id", idCount);
+      col.appendChild(button);
+      idCount++;
+    }
+  }
+  gameBoard.appendChild(table);
 };
+
+// Next steps: on click of button show x or o depending on turn and disable the button.
+// Then, code for determining if there is a winner. Next, display winner or tie.
+// Finally add console statements, comments, and styling. Update README.md pagee.
